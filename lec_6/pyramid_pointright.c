9c98f5e370689fdfcb1bfd42b1c0bf20035cceea
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-09-15 23:03:23 +0530

    Update pyramid_pointright.c

--- lec_6/pyramid_pointright.c
+++ lec_6/pyramid_pointright.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("entr yoour rows");
+    int n,i,j;
+    printf("entr your rows:");
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
-     for (int j=0;j<=i;j++)
+    for(i=0;i<n;i++){
+     for(j=0;j<=i;j++)
       printf("*");
-     for(int k=n-1;k>i;k--)
-      printf(" ");
-     for()  
-     printf("\n");
-    }
-    printf("*");
+     printf("\n"); }
+    for(int k=0;k<(n+1);k++)
+     printf("*");
+    printf("\n");
+    for(i=n;i>0;i--){
+     for(j=0;j<i;j++)
+      printf("*");
+     printf("\n"); }  
     return 0;   
 }
